a9c92d3f352e4f672703b6979f9ce6db82ed1ea0
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-09-19 08:14:35 +0300

    Adds arrays to index.js

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield",
+];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+};
+//console.log(destructivelyAppendCat("Ralph"));
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+};
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+};
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+};
+
+function appendCat(name){
+    return [...cats,name];
+};
+//console.log(appendCat("Broom"));
+
+function prependCat(name){
+    return [name,...cats];
+}
+
+function removeLastCat(){
+    return cats.slice(0, - 1);
+}
+//console.log(removeLastCat())
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
